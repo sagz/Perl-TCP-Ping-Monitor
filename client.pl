c76a94962d43b9fd548673801837b3aa868817b0
Sagar <github@sagz.in>
2013-12-21 12:16:33 -0500

    Init

---
+++ client.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Socket;
+
+chomp(my $hostname  = `hostname`);
+$hostname =~ s/\..*//;
+my $server     = $ARGV[0] || 'localhost';
+my $port       = $ARGV[1] || 42424;
+my $frequency  = $ARGV[2] || 1;
+my $me         = $ARGV[3] || $hostname;
+
+while (1) {
+    print "TCP pinging\n";
+    my $socket = IO::Socket::INET->new(PeerAddr => $server,
+                                PeerPort => $port,
+                                Proto    => "tcp",
+                                Type     => SOCK_STREAM)
+        or die "Couldn't connect to $server:$port : $@\n";
+
+    print "$me: Next ping in $frequency seconds\n";
+    print $socket "$me: Next ping in $frequency seconds\n";
+    my $response = "";
+
+    # Read upto 24 characters 
+    $socket->recv($response, 24);
+    print "$response\n";
+    close $socket;
+    print "sleeping\n";
+    sleep $frequency;
+}
---
+++ server.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Socket;
+use threads;
+use Thread::Queue;
+
+#Defaulting to 8000
+my $port = $ARGV[0] || 42424;
+
+# storing list of connected clients sensors to monitor
+my %clients;
+my $queue = Thread::Queue -> new;
+my $monitor = threads->create ("monitor", $queue);
+
+# This probably might not be of use
+$SIG{CHLD} = 'IGNORE';
+
+my $listen_socket = IO::Socket::INET->new(LocalPort => $port,
+                                          Listen => 10,
+                                          Proto => 'tcp',
+                                          Reuse => 1);
+# Confirm we are listening
+die "Can't listen on socket: $@" unless $listen_socket;
+
+warn "Server ready. Listening to port $port\n";
+
+# Process TCP data after accepting connection
+while (my $connection = $listen_socket->accept) {
+    # spawning a thread per client sensor
+    # Could potentially do this with IO Multiplexing too
+    threads->create ("read_data", $queue, $connection)->detach;
+}
+
+sub read_data {
+    # accept data from the socket and put it on the queue
+    my ($queue, $socket) = @_;
+    while (<$socket>) {
+        print "Received: $_";
+        $queue -> enqueue(time." $_");
+    }
+    print $socket "Ping received by server.\n";
+    shutdown($socket, 1);
+    close $socket
+}
+
+sub monitor {
+    my $queue = shift;
+
+    # As of now, the monitor is invoked only every 30 seconds
+    while (1) {
+        while ($queue -> pending) {
+            my $data = $queue -> dequeue;
+            print "monitor got: $data\n";
+            $data =~ /(\d+) (\S+): Next ping in (\d+) seconds/;
+            my $time       = $1;
+            my $client     = $2;
+            my $frequency  = $3;
+
+            if ((defined $clients{$client}) and $clients{$client} -> [0] eq 'NAK') {
+                print "$client sent a ping again\n";
+            }
+            $clients{$client} = [ 'OK', $time + $frequency];
+        }
+        for my $client (keys %clients) {
+            next if $clients{$client}->[0] eq 'NAK';
+            next if $clients{$client}->[1] > time;
+            print "$client missed a signal ping, expected at $clients{$client}->[1], now it is ".time."\n";
+            $clients{$client}->[0] = 'NAK';
+        }
+        sleep 10;
+    }
+}
